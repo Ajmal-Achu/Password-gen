6285c7fae4e29fcfbef4289c8245b44b6f38ee77
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-08-11 11:47:41 +0530

    RANDOM PASSWORD GENARATOR

---
+++ genarator.py
@@ -0,0 +1,22 @@
+/* Copy Right Credits 2022 @AJMAL
+https://github.com/Ajmal-Achu*/
+
+import random
+
+// values
+
+let lower = "abcdefghijklmnopqrstuvwxyz"
+let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let number = "123456789"
+let symbol = "!@3$%^&*+?,"
+
+// values ends here
+
+// random password genarator
+
+all =lower + upper + number + symbol
+length = 11
+password = "".join(random.sample(all,length))
+
+
+print(" THE RANDOMLY GENARATED PASSWORD IS :" + password)
