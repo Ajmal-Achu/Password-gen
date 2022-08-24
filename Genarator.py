b8332fc71a43d4f2a8836c1c740de17e2744721a
Ajmal <88565942+Ajmal-Achu@users.noreply.github.com>
2022-08-24 22:21:37 +0530

    NOW YOU CAN SELECT THE LENGTH OF THE PASSWORD

--- Genarator.py
+++ Genarator.py
@@ -33,3 +33,27 @@ def replaceWithUppercaseLetter(pword):
         replace_index = random.randrange(len(pword)//2,len(pword))
         pword = pword[0:replace_index] + pword[replace_index].upper() + pword[replace_index+1:]
         return pword
+
+    def main():
+    
+    numPasswords = int(input("How many passwords do you want to generate? "))
+    
+    print("Generating " +str(numPasswords)+" passwords")
+    
+    passwordLengths = []
+
+    print("Minimum length of password should be 3")
+
+    for i in range(numPasswords):
+        length = int(input("Enter the length of Password #" + str(i+1) + " "))
+        if length<3:
+            length = 3
+        passwordLengths.append(length)
+    
+    
+    Password = generatePassword(passwordLengths)
+
+    for i in range(numPasswords):
+        print ("Password #"+str(i+1)+" = " + Password[i])
+
+main()
